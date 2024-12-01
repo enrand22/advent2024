c27807d113e3f98d8b234f36e575d5853a3b3c8f
= <enriqueand22@outlook.com>
2024-12-01 15:25:53 -0600

    create split_lists to create both lists at the same time

--- day_1.exs
+++ day_1.exs
@@ -4,36 +4,38 @@ defmodule DayHelper do
     split = String.split(line)
     [ String.to_integer(Enum.at(split, 0)), String.to_integer(Enum.at(split, 1)) ]
   end
+
+  def split_lists(list) do
+    list
+    |> List.foldl({[], []}, fn elem, acc ->
+      {
+        [ Enum.at(elem, 0) | Kernel.elem(acc, 0) ],
+        [ Enum.at(elem, 1) | Kernel.elem(acc, 1) ]
+      }
+    end)
+  end
 end
 
 defmodule Day1 do
-  def task1_find_total_distance(list) do
-    first_list = list
-    |> Enum.map(&Enum.at(&1, 0))
-    |> Enum.sort
-
-    second_list = list
-    |> Enum.map(&Enum.at(&1, 1))
+  def task1_find_total_distance(first_list, second_list) do
+    second_list = second_list
     |> Enum.sort
 
     first_list
+    |> Enum.sort()
     |> Enum.with_index(fn element, index -> abs(element - Enum.at(second_list, index)) end)
     |> Enum.sum()
     |> IO.inspect
     # 2375403
   end
 
-  def task2_find_similarity(list) do
-    first_list = list
-    |> Enum.map(&Enum.at(&1, 0))
-
-    second_list = list
-    |> Enum.map(&Enum.at(&1, 1))
+  def task2_find_similarity(first_list, second_list) do
+    frecuencies = second_list
     |> Enum.frequencies()
 
     first_list
     |> Enum.map(fn f ->
-      case Map.fetch(second_list, f) do
+      case Map.fetch(frecuencies, f) do
         {:ok, v } -> f * v
         :error    -> 0
       end
@@ -44,9 +46,10 @@ defmodule Day1 do
   end
 end
 
-list = File.read!("inputs/input_day1.txt")
+{list1, list2} = File.read!("inputs/input_day1.txt")
 |> String.split("\n", trim: true)
 |> Enum.map(&DayHelper.split_numbers/1)
+|> DayHelper.split_lists()
 
-Day1.task1_find_total_distance(list)
-Day1.task2_find_similarity(list)
+Day1.task1_find_total_distance(list1, list2)
+Day1.task2_find_similarity(list1, list2)
