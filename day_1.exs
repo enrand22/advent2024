098caa6a5b09d7667bb5972200a1ccba59fa6f9f
= <enriqueand22@outlook.com>
2024-12-02 21:03:39 -0600

    Use of core funciton

--- day_1.exs
+++ day_1.exs
@@ -2,17 +2,7 @@
 defmodule DayHelper do
   def split_numbers(line) do
     split = String.split(line)
-    [ String.to_integer(Enum.at(split, 0)), String.to_integer(Enum.at(split, 1)) ]
-  end
-
-  def split_lists(list) do
-    list
-    |> List.foldl({[], []}, fn elem, acc ->
-      {
-        [ Enum.at(elem, 0) | Kernel.elem(acc, 0) ],
-        [ Enum.at(elem, 1) | Kernel.elem(acc, 1) ]
-      }
-    end)
+    { String.to_integer(Enum.at(split, 0)), String.to_integer(Enum.at(split, 1)) }
   end
 end
 
@@ -43,7 +33,7 @@ end
 {list1, list2} = File.read!("inputs/input_day1.txt")
 |> String.split("\n", trim: true)
 |> Enum.map(&DayHelper.split_numbers/1)
-|> DayHelper.split_lists()
+|> Enum.unzip
 
 Day1.task1_find_total_distance(list1, list2 |> Enum.sort() )
 Day1.task2_find_similarity(list1,     list2 |> Enum.frequencies() )
