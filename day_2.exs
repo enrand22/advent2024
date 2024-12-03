3e7af1b01957fb83b65a6870c6f3cb204870ca36
= <enriqueand22@outlook.com>
2024-12-02 21:26:06 -0600

    refactor

--- day_2.exs
+++ day_2.exs
@@ -1,61 +1,46 @@
 defmodule DayHelper do
   def calc_each_report(report) do
     [head | tail ] = report
-
     distances = calc_distance(head, [], tail)
 
-    all_positive = test_positivity(distances)
-    all_negative = test_negativity(distances)
-
-    if all_positive || all_negative do
-      Enum.map(distances, fn x -> abs(x) end)
-      |> Enum.all?(fn x -> x >= 1 && x <= 3 end)
-    else
-      false
-    end
+    test_signed_similarity(distances) && test_distance_threshold(distances)
   end
 
   def calc_each_report_with_dampness(report, recalc \\ true) do
     [head | tail ] = report
-
     distances = calc_distance(head, [], tail)
 
-    all_positive = test_positivity(distances)
-    all_negative = test_negativity(distances)
-
-    if all_positive || all_negative do
-      result = Enum.map(distances, fn x -> abs(x) end)
-      |> Enum.all?(fn x -> x >= 1 && x <= 3 end)
-
-      if result do
-        true
-      else
-         if recalc do
-          re_test(0, Enum.count(report), report, false)
-         else
-          false
-         end
-      end
+    if test_signed_similarity(distances) && test_distance_threshold(distances) do
+      true
     else
       if recalc do
-        re_test(0, Enum.count(report), report, false)
+        re_test(0, report, false)
       else
         false
       end
     end
   end
 
-  def re_test(_index, _length, _original, safeness) when safeness, do: true
+  def re_test(_index, _original, safeness) when safeness, do: true
 
-  def re_test(index, length, original, _safeness) when index < length do
+  def re_test(index, original, _safeness) when index < length(original) do
     {_, new_report} = List.pop_at(original, index)
-    re_test(index + 1, length, original, calc_each_report_with_dampness(new_report, false ) )
+    re_test(index + 1, original, calc_each_report_with_dampness(new_report, false))
   end
 
-  def re_test(_index, _length, _original, _safeness), do: false
+  def re_test(_index, _original, _safeness), do: false
 
   def test_positivity(distances), do: Enum.all?(distances, fn x -> x >= 0 end)
   def test_negativity(distances), do: Enum.all?(distances, fn x -> x <= 0 end)
+
+  def test_signed_similarity(distances) do
+    if test_positivity(distances), do: true, else: test_negativity(distances)
+  end
+
+  def test_distance_threshold(distances) do
+    Enum.map(distances, fn x -> abs(x) end)
+    |> Enum.all?(fn x -> x >= 1 && x <= 3 end)
+  end
 
   def calc_distance(past_value, new_list, [head | tail]) when length(tail) == 0 do
     [past_value - head | new_list ]
@@ -73,10 +58,14 @@ defmodule DayHelper do
 end
 
 defmodule Day2 do
-  def task1_find_safeness do
+  def prepare_file() do
     File.read!("inputs/input_day2.txt")
     |> String.split("\n", trim: true)
     |> Enum.map(&DayHelper.split_numbers/1)
+  end
+
+  def task1_find_safeness do
+    prepare_file()
     |> Enum.map(&DayHelper.calc_each_report/1)
     |> Enum.count(fn x -> x end)
     |> IO.inspect()
@@ -84,13 +73,11 @@ defmodule Day2 do
   end
 
   def task2_find_safeness_with_dampeness do
-    File.read!("inputs/input_day2.txt")
-    |> String.split("\n", trim: true)
-    |> Enum.map(&DayHelper.split_numbers/1)
+    prepare_file()
     |> Enum.map(&DayHelper.calc_each_report_with_dampness/1)
     |> Enum.count(fn x -> x end)
     |> IO.inspect()
-    #242
+    #311
   end
 
 end
